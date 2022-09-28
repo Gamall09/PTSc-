8e492ddc03365688f9804ddc8935f952699fce78
Gamall09 <111164568+Gamall09@users.noreply.github.com>
2022-09-28 17:55:38 +0700

    Add files via upload

---
+++ MSyahGamal_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+
+cout << "Nama  : M. Syah Gamal Rosyada"<< endl;
+cout << "Kelas : X RPL 1" << endl;
+cout << "Nilai : 92" << endl;
+
+}
---
+++ MSyahGamal_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nilai Uts = " ;
+    int angka1;
+    cin >> angka1;
+    
+    cout << "Nilai Uas = ";
+    int angka2;
+    cin >> angka2;
+
+    int hasil;
+    hasil = angka1 + angka2;
+    cout << "hasil penjumlahan =" <<hasil;
+    }
